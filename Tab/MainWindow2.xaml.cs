25fd41e15d9aa2dbbf15b3eef0d851b086f79168
manyanxp <manyanxp.cat.com@gmail.com>
2018-02-13 22:44:19 +0900

    Tab改良

---
+++ Tab/MainWindow2.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class ContentToMarginConverter : IValueConverter
+    {
+        #region IValueConverter Members
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return new Thickness(0, 0, -((ContentPresenter)value).ActualHeight, 0);
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+
+    public class ContentToPathConverter : IValueConverter
+    {
+        #region IValueConverter Members
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var ps = new PathSegmentCollection(4);
+            ContentPresenter cp = (ContentPresenter)value;
+            double h = cp.ActualHeight > 10 ? 1.4 * cp.ActualHeight : 10;
+            double w = cp.ActualWidth > 10 ? 1.25 * cp.ActualWidth : 10;
+            //ps.Add(new LineSegment(new Point(1, 0.7 * h), true));
+            //ps.Add(new BezierSegment(new Point(1, 0.9 * h), new Point(0.1 * h, h), new Point(0.3 * h, h), true));
+            //ps.Add(new LineSegment(new Point(w, h), true));
+            //ps.Add(new BezierSegment(new Point(w + 0.6 * h, h), new Point(w + h, 0), new Point(w + h * 1.3, 0), true));
+            ps.Add(new LineSegment(new Point(1, 0), true));
+            ps.Add(new LineSegment(new Point(5, 0.7 * h ), true));
+            ps.Add(new LineSegment(new Point(w + 5, 0.7 * h), true));
+            ps.Add(new LineSegment(new Point(w + 10, 0), true));
+            //ps.Add(new BezierSegment(new Point(1, 0.9 * h), new Point(0.1 * h, h), new Point(0.3 * h, h), true));
+            //ps.Add(new LineSegment(new Point(w, h), true));
+            //ps.Add(new BezierSegment(new Point(w + 0.6 * h, h), new Point(w + h, 0), new Point(w + h * 1.3, 0), true));
+
+            return ps;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
